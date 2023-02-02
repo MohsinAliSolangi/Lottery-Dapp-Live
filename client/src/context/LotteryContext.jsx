8c0f96b86fd46fe3f3593bc94700c593e9bb7158
mohsinpluton <mohsin.pluton@gmail.com>
2023-02-02 18:59:06 +0500

    first commit

---
+++ client/src/context/LotteryContext.jsx
@@ -0,0 +1,165 @@
+import { ethers } from "ethers";
+import React, { useState, useEffect, createContext } from "react";
+
+import { lotteryAbi, contractAddress } from "./utils/constants";
+
+export const LotteryContext = createContext();
+
+const { ethereum } = window;
+
+export const getLotteryContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const LotteryContract = new ethers.Contract(contractAddress, lotteryAbi, signer);
+    return LotteryContract;
+
+}
+
+export const LotteryProvider = ({ children }) => {
+    const [currentAccount, setCurrentAccount] = useState();
+    const [lotteryPot, setLotteryPot] = useState();
+    const [lotteryPlayers, setLotteryPlayers] = useState([]);
+    const [lotteryHistory, setLotteryHistory] = useState([]);
+    const [lotteryId,setLotteryId] = useState();
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        checkWalletIsConnected();
+        updateState();
+    }, [])
+
+    ethereum.on("accountsChanged", async(account) => {
+        setCurrentAccount(account[0]);
+    })
+
+
+    const updateState = () => {
+        if (getLotteryContract()) getPot();
+        if (getLotteryContract()) getPlayers();
+        if (getLotteryContract()) getLotteryId();
+    }
+
+
+    const checkWalletIsConnected = async () => {
+        try {
+            if (!ethereum) return alert("Please Install Metamask");
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+            if (accounts.length) {
+                setCurrentAccount(accounts[0]);
+
+            } else {
+                console.log("No account found")
+            }
+      
+        } catch (err) {
+            setError(err.message);
+            throw new Error("No ethereum object.")
+        }
+    }
+
+
+    const connectWallet = async () => {
+        setError("")
+        try {
+            setLoading(true)
+            if (!ethereum) return alert("Please Install Metamask");
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+            setCurrentAccount(accounts[0]);
+            window.location.reload();
+            setLoading(false)
+
+        } 
+        
+        catch (err) {
+            setLoading(false)
+            setError(err.message);
+            throw new Error("No ethereum object.")
+        }
+    }
+
+
+    const getPot = async () => {
+        const pot = await getLotteryContract().getBalance();
+        setLotteryPot(ethers.utils.formatEther(pot));
+        
+    }
+
+    const getPlayers = async () => {
+        const players = await getLotteryContract().getparticipants()
+        setLotteryPlayers(players);
+    }
+
+    const enterLottery = async () => {
+        
+        setError("")
+        try {
+            setLoading(true)    
+          const res =  await getLotteryContract().participant({
+                from: currentAccount,
+                value: '6',
+                gasLimit: 3000000,
+                gasPrice: null
+
+        })
+        await res.wait()
+        window.location.reload()
+        setLoading(false)
+
+        } catch (err) {
+        setLoading(false)
+            setError(err.message);
+        }
+
+    }
+
+    const reload = () => {
+        window.location.reload()
+    }
+
+    const pickWinner =  async() => {
+
+       setError('')
+       setSuccess('')
+        try{
+        setLoading(true)
+           const res =  await getLotteryContract().openLottery({
+                from: currentAccount,
+                gasLimit: 3000000,
+                gasPrice: null
+            })
+            await res.wait()
+        setLoading(false)
+        }catch(err){
+        setLoading(false)
+            setError(err.message);
+        }
+    }
+    
+    const getLotteryId = async() => {
+        const id = await getLotteryContract().result()
+       if(id.toString() !== "0"){
+        setLotteryId(parseInt(id));
+        await getLotteryHistory(parseInt(id))
+    }
+    }
+
+    const getLotteryHistory = async(id) => {
+        const winnerAddress = await getLotteryContract().participants(id);
+        const historyObj = {};
+        historyObj.id = id;
+        historyObj.address = winnerAddress;
+        setLotteryHistory(lotteryHistory => [...lotteryHistory,historyObj]);
+    }
+
+
+    return (
+        <LotteryContext.Provider value={{ connectWallet, currentAccount, lotteryPot, lotteryPlayers, enterLottery,error,success,pickWinner,lotteryHistory,lotteryId,loading }}>
+            {children}
+        </LotteryContext.Provider>
+    )
+    }
+
+
---
+++ client/src/context/utils/constants.js
@@ -0,0 +1,235 @@
+export const lotteryAbi = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint64",
+        "name": "subscriptionId",
+        "type": "uint64"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "have",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "want",
+        "type": "address"
+      }
+    ],
+    "name": "OnlyCoordinatorCanFulfill",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "requestId",
+        "type": "uint256"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "result",
+        "type": "uint256"
+      }
+    ],
+    "name": "DiceLanded",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "requestId",
+        "type": "uint256"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "roller",
+        "type": "address"
+      }
+    ],
+    "name": "DiceRolled",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "Lottery",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBalance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getparticipants",
+    "outputs": [
+      {
+        "internalType": "address payable[]",
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "openLottery",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "requestId",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "participant",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "participants",
+    "outputs": [
+      {
+        "internalType": "address payable",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "requestId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "randomWords",
+        "type": "uint256[]"
+      }
+    ],
+    "name": "rawFulfillRandomWords",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "request",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "requestIds",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "result",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "results",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+export const contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+
